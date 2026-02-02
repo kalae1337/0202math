3591cf7431eec2023e05aab4f4866a7afc0bdb86
Kalae <kalae1337@gmail.com>
2026-02-02 16:18:12 +0100

    converting

---
+++ src/Konvertalas.java
@@ -0,0 +1,16 @@
+public class Konvertalas {
+    public static void csinald() {
+        String szoveg01 = "45";
+        int szam01 = Integer.parseInt(szoveg01);
+
+        int szam02 = 30;
+        String szoveg02 = String.valueOf(szam02);
+
+        String szoveg03 = "43.2354";
+        double szam03 = Double.parseDouble(szoveg03);
+        
+        System.out.println(szam01 + " " + szoveg02 + " " + szam03);
+
+        
+    }
+}
