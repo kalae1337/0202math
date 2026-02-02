a9e8412f8f5e9f70bddd8b2ca33adf0d7bc16bc9
Kalae <kalae1337@gmail.com>
2026-02-02 15:39:35 +0100

    poker javitva

--- src/Poker.java
+++ src/Poker.java
@@ -2,24 +2,17 @@ import java.util.Random;
 
 public class Poker {
     public static void play() {
-        playHuman();
-        playMachine();
+        playerThrow("Ember");
+        playerThrow("Gep");
     }
-    public static void playHuman() {
+    
+    public static void playerThrow(String role) {
         Random rnd = new Random();
 
-        System.out.print("ember:\t");
+        System.out.print(role +":\t");
         for(int i = 1; i< 6; i++) {
             System.out.print(rnd.nextInt(6)+1 + " ");
         }
         System.out.println();
-    }
-    public static void playMachine() {
-        Random rnd = new Random();
-
-        System.out.print("gep:\t");
-        for(int i = 1; i< 6; i++) {
-            System.out.print(rnd.nextInt(6)+1 + " ");
-        }
 }
 }
