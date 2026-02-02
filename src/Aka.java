9735625422935ac9037e855344cfb0756e128511
Kalae <kalae1337@gmail.com>
2026-02-02 16:36:03 +0100

    format final

--- src/Aka.java
+++ src/Aka.java
@@ -1,5 +1,6 @@
 public class Aka {
     public static void task() {
+        System.out.println("Kalae, i1, 26/02/02\nszit 0201 feladat ");
         double a = 583.123456789;
         System.out.printf("%20.3f\n", a );
     }
