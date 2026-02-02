9c371fae24a9861e4c6dc6a2d7147dd8bbde2b30
Kalae <kalae1337@gmail.com>
2026-02-02 15:33:52 +0100

    poker javitva

--- src/Poker.java
+++ src/Poker.java
@@ -2,12 +2,24 @@ import java.util.Random;
 
 public class Poker {
     public static void play() {
+        playHuman();
+        playMachine();
+    }
+    public static void playHuman() {
         Random rnd = new Random();
-        //2 ugyanolyan nyerobb, ket par, harom ugyanolyan
-        //5 db egymas mellett
-        System.out.print("Kockapoker: ");
+
+        System.out.print("ember:\t");
         for(int i = 1; i< 6; i++) {
             System.out.print(rnd.nextInt(6)+1 + " ");
         }
+        System.out.println();
     }
+    public static void playMachine() {
+        Random rnd = new Random();
+
+        System.out.print("gep:\t");
+        for(int i = 1; i< 6; i++) {
+            System.out.print(rnd.nextInt(6)+1 + " ");
+        }
+}
 }
